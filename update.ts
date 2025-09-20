bea6df45b9f55ef3d9fbde3992a759bbf33d7f42
Tyler Tierney <tytierney@yahoo.com>
2025-09-20 13:12:17 -0400

    change update.ts url; add a console log

--- update.ts
+++ update.ts
@@ -1,75 +1,77 @@
-import connections from "./connections.json";
-import { Answer, Game } from "./src/models/models";
-import fs from "fs";
+import connections from './connections.json'
+import { Answer, Game } from './src/models/models'
+import fs from 'fs'
 
 const convertNytGameToConnectionsGame = (
   nytGame: NyTimesGame,
   date: string,
   id: number
 ): Game => {
-  const answers: Answer[] = [];
+  const answers: Answer[] = []
 
   for (const group in nytGame.groups) {
     const answer: Answer = {
       level: nytGame.groups[group].level,
       group: group,
       members: nytGame.groups[group].members,
-    };
-    answers.push(answer);
+    }
+    answers.push(answer)
   }
 
-  const game: Game = { id, date, answers };
-  return game;
-};
+  const game: Game = { id, date, answers }
+  return game
+}
 
 interface NyTimesGame {
-  id: number;
+  id: number
   groups: {
     [key: string]: {
-      level: number;
-      members: string[];
-    };
-  };
-  startingGroups: string[][];
+      level: number
+      members: string[]
+    }
+  }
+  startingGroups: string[][]
 }
 
 const getNewGame = async () => {
-  const mostRecentGame = connections.at(-1);
-  if (!mostRecentGame) return;
+  const mostRecentGame = connections.at(-1)
+  if (!mostRecentGame) return
 
-  const today = new Date();
+  const today = new Date()
 
-  const date = new Intl.DateTimeFormat("en-CA", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  }).format(today);
+  const date = new Intl.DateTimeFormat('en-CA', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).format(today)
 
   if (date === mostRecentGame.date) {
     console.log(
       `Connection game #${mostRecentGame.id} from ${mostRecentGame.date} already exists.`
-    );
-    return;
+    )
+    return
   }
 
-  const url = `https://www.nytimes.com/svc/connections/v1/${date}.json`;
+  const url = `https://www.nytimes.com/svc/connections/v2/${date}.json`
 
   try {
-    const response = await fetch(url);
-    const data = (await response.json()) as NyTimesGame;
+    const response = await fetch(url)
+    const data = (await response.json()) as NyTimesGame
+
+    console.log(data)
 
     const game = convertNytGameToConnectionsGame(
       data,
       date,
       mostRecentGame.id + 1
-    );
-    connections.push(game);
+    )
+    connections.push(game)
 
-    fs.writeFileSync("connections.json", JSON.stringify(connections, null, 2));
+    fs.writeFileSync('connections.json', JSON.stringify(connections, null, 2))
   } catch (err) {
-    console.log(`Failed to fetch newest game`, err);
-    return;
+    console.log(`Failed to fetch newest game`, err)
+    return
   }
-};
+}
 
-getNewGame();
+getNewGame()
